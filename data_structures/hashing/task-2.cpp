8b393fd9347cee4752e9ffa345e2a341400b62e6
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-03-05 17:24:08 -0800

    style(ds.hashing): remove bonus task since its unrelated to hashing

--- data_structures/hashing/task-2.cpp
+++ data_structures/hashing/task-2.cpp
@@ -19,28 +19,6 @@ void hashInput(std::string in) {
   std::cout << "Hash value of \"" << in << "\": " << hashValue << std::endl;
 }
 
-void bounus_task(std::string in) {
-  // Create an unordered set of strings
-  std::unordered_set<std::string> names;
-
-  names.insert("Alice");
-  names.insert("Bob");
-
-  if (names.find(in) != names.end()) {
-    std::cout << in << " is in the set!" << std::endl;
-  } else {
-    std::cout << in << " is not in the set." << std::endl;
-    std::cout << "Adding " << in << " to the set." << std::endl;
-    names.insert(in);
-    std::cout << in << " has been added to the set." << std::endl;
-  }
-
-  std::cout << "Set contents:" << std::endl;
-  for (const auto &name : names) {
-    std::cout << name << std::endl;
-  }
-}
-
 int main() {
 
   std::string input = userInput();
@@ -49,9 +27,6 @@ int main() {
   std::cout << "Task 2:" << std::endl;
   hashInput(input);
   std::cout << "---" << std::endl;
-
-  // std::cout << "Bonus Task:" << std::endl;
-  // bounus_task(input);
 
   return 0;
 }
