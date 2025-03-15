1e0facf444d21bbd4fb1a8a45b3bde63519947c6
Sebastian Yaghoubi <sebastianyaghoubi@gmail.com>
2025-03-15 16:54:10 -0700

    feat(ds.linked-lists): use generic node types with templates

--- data_structures/linked_lists/linked_list.cpp
+++ data_structures/linked_lists/linked_list.cpp
@@ -6,29 +6,28 @@
  */
 
 #include <iostream>
+#include <string>
 
 /* SINGLY LINKED LIST IMPLEMENTATION */
 
 /**
- * Define the structure for a node in a singly linked list
- * Each node contains an integer value and a pointer to the next node
+ * Template definition for a node in a singly linked list
+ * Each node contains a value of type T and a pointer to the next node
  */
-struct ListNode {
-  // The value stored in this node
-  int data;
-  // Pointer to the next node in the list (or nullptr if this is the last node)
-  ListNode *next;
+template <typename T> struct ListNode {
+  T data;            // The value stored in this node, can be any type T
+  ListNode<T> *next; // Pointer to the next node in the list
 };
 
 /**
  * Creates a new node for a singly linked list
  *
- * @param value The integer to store in the new node
+ * @param value The value of type T to store in the new node
  * @return Pointer to the newly created node
  */
-ListNode *createListNode(int value) {
+template <typename T> ListNode<T> *createListNode(T value) {
   // Allocate memory for a new ListNode
-  ListNode *newNode = new ListNode;
+  ListNode<T> *newNode = new ListNode<T>;
   // Initialize the node's data field with the provided value
   newNode->data = value;
   // Initialize the next pointer to nullptr (indicating no next node yet)
@@ -40,13 +39,12 @@ ListNode *createListNode(int value) {
 /**
  * Adds a new node at the beginning of a singly linked list
  *
- * @param headRef Pointer to the head pointer of the list (allows modification
- * of the head)
- * @param value The integer value to store in the new node
+ * @param headRef Pointer to the head pointer of the list
+ * @param value The value of type T to store in the new node
  */
-void addToStartOfList(ListNode **headRef, int value) {
+template <typename T> void addToStartOfList(ListNode<T> **headRef, T value) {
   // Create a new node with the given value
-  ListNode *newNode = createListNode(value);
+  ListNode<T> *newNode = createListNode(value);
 
   /* Make the new node point to the current head of the list
    * This connects the new node to the existing list */
@@ -64,20 +62,19 @@ void addToStartOfList(ListNode **headRef, int value) {
 /**
  * Adds a new node at the end of a singly linked list
  *
- * @param headRef Pointer to the head pointer of the list (allows modification
- * of the head if the list is empty)
- * @param value The integer value to store in the new node
+ * @param headRef Pointer to the head pointer of the list
+ * @param value The value of type T to store in the new node
  */
-void addToEndOfList(ListNode **headRef, int value) {
+template <typename T> void addToEndOfList(ListNode<T> **headRef, T value) {
   // Create a new node with the given value
-  ListNode *newNode = createListNode(value);
+  ListNode<T> *newNode = createListNode(value);
 
   // If the list is empty, the new node becomes the head
   if (*headRef == nullptr) {
     *headRef = newNode;
   } else {
     // Otherwise, find the last node
-    ListNode *current = *headRef;
+    ListNode<T> *current = *headRef;
     while (current->next != nullptr) {
       current = current->next;
     }
@@ -93,10 +90,9 @@ void addToEndOfList(ListNode **headRef, int value) {
 /**
  * Deletes the first node from a singly linked list
  *
- * @param headRef Pointer to the head pointer of the list (allows modification
- * of the head)
+ * @param headRef Pointer to the head pointer of the list
  */
-void deleteAtStartOfList(ListNode **headRef) {
+template <typename T> void deleteAtStartOfList(ListNode<T> **headRef) {
   // Check if the list is empty (head points to nullptr)
   if (*headRef == nullptr) {
     // If the list is empty, output a message and return without doing anything
@@ -106,14 +102,14 @@ void deleteAtStartOfList(ListNode **headRef) {
 
   /* Store the current head node in a temporary pointer
    * This allows us to delete it after updating the head */
-  ListNode *temp = *headRef;
+  ListNode<T> *temp = *headRef;
 
   // Update the head to point to the second node
   // This removes the first node from the list structure
   *headRef = (*headRef)->next;
 
   // Save the value of the node being deleted for the confirmation message
-  int deletedValue = temp->data;
+  auto deletedValue = temp->data;
 
   // Free the memory allocated for the old head node
   delete temp;
@@ -126,10 +122,9 @@ void deleteAtStartOfList(ListNode **headRef) {
 /**
  * Deletes the last node from a singly linked list
  *
- * @param headRef Pointer to the head pointer of the list (allows modification
- * of the head in case the list has only one node)
+ * @param headRef Pointer to the head pointer of the list
  */
-void deleteAtEndOfList(ListNode **headRef) {
+template <typename T> void deleteAtEndOfList(ListNode<T> **headRef) {
   // Check if the list is empty
   if (*headRef == nullptr) {
     std::cout << "List is empty. Nothing to delete." << std::endl;
@@ -140,7 +135,7 @@ void deleteAtEndOfList(ListNode **headRef) {
    * This is equivalent to deleting from the start */
   if ((*headRef)->next == nullptr) {
     // Store the value for the confirmation message
-    int deletedValue = (*headRef)->data;
+    auto deletedValue = (*headRef)->data;
     // Delete the only node
     delete *headRef;
     // Set head to nullptr (empty list)
@@ -154,8 +149,8 @@ void deleteAtEndOfList(ListNode **headRef) {
   /* For a list with multiple nodes, we need to find the second-to-last node
    * We'll use two pointers: current will eventually point to the second-to-last
    * node, and temp will point to the last node */
-  ListNode *current = *headRef;
-  ListNode *temp = current->next;
+  ListNode<T> *current = *headRef;
+  ListNode<T> *temp = current->next;
 
   // Navigate to the second-to-last node
   while (temp->next != nullptr) {
@@ -164,7 +159,7 @@ void deleteAtEndOfList(ListNode **headRef) {
   }
 
   // Store the value of the node being deleted for the confirmation message
-  int deletedValue = temp->data;
+  auto deletedValue = temp->data;
 
   // Delete the last node
   delete temp;
@@ -180,10 +175,11 @@ void deleteAtEndOfList(ListNode **headRef) {
 
 /**
  * Displays all elements in a singly linked list
+ * Requires the type T to have operator<< defined
  *
  * @param head Pointer to the first node in the list
  */
-void displayList(ListNode *head) {
+template <typename T> void displayList(ListNode<T> *head) {
   // Check if the list is empty
   if (head == nullptr) {
     std::cout << "List is empty" << std::endl;
@@ -194,7 +190,7 @@ void displayList(ListNode *head) {
   std::cout << "List elements: ";
 
   // Initialize current to the first node to start traversal
-  ListNode *current = head;
+  ListNode<T> *current = head;
 
   /* Traverse the list from head to tail
    * Print each element and follow the next pointers */
@@ -222,11 +218,11 @@ void displayList(ListNode *head) {
  * @param headRef Pointer to the head pointer of the list (allows setting head
  * to nullptr)
  */
-void freeList(ListNode **headRef) {
+template <typename T> void freeList(ListNode<T> **headRef) {
   // current initially points to the first node
-  ListNode *current = *headRef;
+  ListNode<T> *current = *headRef;
   // next will temporarily store the next node during deletion
-  ListNode *next;
+  ListNode<T> *next;
 
   /* Traverse the list and delete each node
    * We need to save the next pointer before deleting the current node */
@@ -246,25 +242,25 @@ void freeList(ListNode **headRef) {
 /*  DOUBLY LINKED LIST IMPLEMENTATION */
 
 /**
- * Define the structure for a node in a doubly linked list
- * Each node contains an integer value and pointers to both next and previous
+ * Template definition for a node in a doubly linked list
+ * Each node contains a value of type T and pointers to both next and previous
  * nodes
  */
-struct DListNode {
-  int data;        // The value stored in this node
-  DListNode *next; // Pointer to the next node in the list
-  DListNode *prev; // Pointer to the previous node in the list
+template <typename T> struct DListNode {
+  T data;             // The value stored in this node, can be any type T
+  DListNode<T> *next; // Pointer to the next node in the list
+  DListNode<T> *prev; // Pointer to the previous node in the list
 };
 
 /**
  * Creates a new node for a doubly linked list
  *
- * @param value The integer to store in the new node
+ * @param value The value of type T to store in the new node
  * @return Pointer to the newly created node
  */
-DListNode *createDListNode(int value) {
+template <typename T> DListNode<T> *createDListNode(T value) {
   // Allocate memory for a new DListNode
-  DListNode *newNode = new DListNode;
+  DListNode<T> *newNode = new DListNode<T>;
   // Initialize the node's data field with the provided value
   newNode->data = value;
   // Initialize the next pointer to nullptr (indicating no next node yet)
@@ -282,9 +278,11 @@ DListNode *createDListNode(int value) {
  * @param tailRef Pointer to the tail pointer of the list
  * @param value The integer value to store in the new node
  */
-void addToStartOfDList(DListNode **headRef, DListNode **tailRef, int value) {
+template <typename T>
+void addToStartOfDList(DListNode<T> **headRef, DListNode<T> **tailRef,
+                       T value) {
   // Create a new node with the given value
-  DListNode *newNode = createDListNode(value);
+  DListNode<T> *newNode = createDListNode(value);
 
   /* If the list is empty, the new node becomes both head and tail
    * This handles the special case of the first element */
@@ -315,9 +313,10 @@ void addToStartOfDList(DListNode **headRef, DListNode **tailRef, int value) {
  * @param tailRef Pointer to the tail pointer of the list
  * @param value The integer value to store in the new node
  */
-void addToEndOfDList(DListNode **headRef, DListNode **tailRef, int value) {
+template <typename T>
+void addToEndOfDList(DListNode<T> **headRef, DListNode<T> **tailRef, T value) {
   // Create a new node with the given value
-  DListNode *newNode = createDListNode(value);
+  DListNode<T> *newNode = createDListNode(value);
 
   /* If the list is empty, the new node becomes both head and tail
    * This handles the special case of the first element */
@@ -345,7 +344,8 @@ void addToEndOfDList(DListNode **headRef, DListNode **tailRef, int value) {
  * @param headRef Pointer to the head pointer of the list
  * @param tailRef Pointer to the tail pointer of the list
  */
-void deleteAtStartOfDList(DListNode **headRef, DListNode **tailRef) {
+template <typename T>
+void deleteAtStartOfDList(DListNode<T> **headRef, DListNode<T> **tailRef) {
   // Check if the list is empty
   if (*headRef == nullptr) {
     std::cout << "List is empty. Nothing to delete." << std::endl;
@@ -353,10 +353,10 @@ void deleteAtStartOfDList(DListNode **headRef, DListNode **tailRef) {
   }
 
   // Store the current head node in a temporary pointer
-  DListNode *temp = *headRef;
+  DListNode<T> *temp = *headRef;
 
   // Save the value of the node being deleted for the confirmation message
-  int deletedValue = temp->data;
+  auto deletedValue = temp->data;
 
   /* If there's only one node in the list
    * We need to handle this special case by setting both head and tail to
@@ -388,7 +388,8 @@ void deleteAtStartOfDList(DListNode **headRef, DListNode **tailRef) {
  * @param headRef Pointer to the head pointer of the list
  * @param tailRef Pointer to the tail pointer of the list
  */
-void deleteAtEndOfDList(DListNode **headRef, DListNode **tailRef) {
+template <typename T>
+void deleteAtEndOfDList(DListNode<T> **headRef, DListNode<T> **tailRef) {
   // Check if the list is empty
   if (*tailRef == nullptr) {
     std::cout << "List is empty. Nothing to delete." << std::endl;
@@ -396,10 +397,10 @@ void deleteAtEndOfDList(DListNode **headRef, DListNode **tailRef) {
   }
 
   // Store the current tail node in a temporary pointer
-  DListNode *temp = *tailRef;
+  DListNode<T> *temp = *tailRef;
 
   // Store the value of the node being deleted for the confirmation message
-  int deletedValue = temp->data;
+  auto deletedValue = temp->data;
 
   /* If there's only one node in the list
    * We need to handle this special case by setting both head and tail to
@@ -430,7 +431,7 @@ void deleteAtEndOfDList(DListNode **headRef, DListNode **tailRef) {
  *
  * @param head Pointer to the first node in the list
  */
-void displayForwardDList(DListNode *head) {
+template <typename T> void displayForwardDList(DListNode<T> *head) {
   // Check if the list is empty
   if (head == nullptr) {
     std::cout << "List is empty" << std::endl;
@@ -441,7 +442,7 @@ void displayForwardDList(DListNode *head) {
   std::cout << "List elements (forward): ";
 
   // Initialize current to the first node to start traversal
-  DListNode *current = head;
+  DListNode<T> *current = head;
 
   /* Traverse the list from head to tail
    * Print each element as we go */
@@ -466,7 +467,7 @@ void displayForwardDList(DListNode *head) {
  *
  * @param tail Pointer to the last node in the list
  */
-void displayBackwardDList(DListNode *tail) {
+template <typename T> void displayBackwardDList(DListNode<T> *tail) {
   // Check if the list is empty
   if (tail == nullptr) {
     std::cout << "List is empty" << std::endl;
@@ -477,7 +478,7 @@ void displayBackwardDList(DListNode *tail) {
   std::cout << "List elements (backward): ";
 
   // Initialize current to the last node to start reverse traversal
-  DListNode *current = tail;
+  DListNode<T> *current = tail;
 
   /* Traverse the list from tail to head
    * Following the prev pointers backward through the list */
@@ -503,11 +504,12 @@ void displayBackwardDList(DListNode *tail) {
  * @param headRef Pointer to the head pointer of the list
  * @param tailRef Pointer to the tail pointer of the list
  */
-void freeDList(DListNode **headRef, DListNode **tailRef) {
+template <typename T>
+void freeDList(DListNode<T> **headRef, DListNode<T> **tailRef) {
   // current initially points to the first node
-  DListNode *current = *headRef;
+  DListNode<T> *current = *headRef;
   // next will temporarily store the next node during deletion
-  DListNode *next;
+  DListNode<T> *next;
 
   /* Traverse the list and delete each node
    * Similar to the singly linked list cleanup */
@@ -527,70 +529,81 @@ void freeDList(DListNode **headRef, DListNode **tailRef) {
 
 int main() {
   /* Singly Linked List operations */
-  std::cout << "=== Linked List ===" << std::endl;
+  std::cout << "=== Integer Linked List ===" << std::endl;
   // Initialize head pointer for singly linked list to nullptr (empty list)
-  ListNode *singlyHead = nullptr;
+  ListNode<int> *intHead = nullptr;
 
   /**
    * Add three nodes to the beginning of the list
    * After these operations, the list will contain: 10 -> 20 -> 30
    */
-  addToStartOfList(&singlyHead, 30); // List becomes: 30
-  addToStartOfList(&singlyHead, 20); // List becomes: 20 -> 30
-  addToStartOfList(&singlyHead, 10); // List becomes: 10 -> 20 -> 30
+  addToStartOfList(&intHead, 30); // List becomes: 30
+  addToStartOfList(&intHead, 20); // List becomes: 20 -> 30
+  addToStartOfList(&intHead, 10); // List becomes: 10 -> 20 -> 30
 
   // Display the current state of the list
-  displayList(singlyHead); // Outputs: List elements: 10 -> 20 -> 30
+  displayList(intHead); // Outputs: List elements: 10 -> 20 -> 30
 
-  /**
-   * Delete two nodes from the beginning of the list
-   * This demonstrates removing elements from the singly linked list
-   */
-  deleteAtStartOfList(&singlyHead); // Removes 10, list becomes: 20 -> 30
-  displayList(singlyHead);          // Outputs: List elements: 20 -> 30
-  deleteAtStartOfList(&singlyHead); // Removes 20, list becomes: 30
-  displayList(singlyHead);          // Outputs: List elements: 30
+  // Delete node from the beginning of the list
+  deleteAtStartOfList(&intHead); // Removes 10, list becomes: 20 -> 30
+  displayList(intHead);          // Outputs: List elements: 20 -> 30
+
+  // Delete node from the end of the list
+  deleteAtEndOfList(&intHead); // Removes 30, list becomes: 20
+  displayList(intHead);        // Outputs: List elements: 20
 
   // Free all memory used by the singly linked list
-  freeList(&singlyHead);
+  freeList(&intHead);
 
   // Add a blank line between demonstrations
   std::cout << std::endl;
 
+  // Demonstrate Singly Linked List with strings
+  std::cout << "=== String Linked List ===" << std::endl;
+  ListNode<std::string> *stringHead = nullptr;
+
+  addToStartOfList(&stringHead, std::string("world"));
+  addToStartOfList(&stringHead, std::string("hello"));
+  displayList(stringHead);
+
+  std::cout << std::endl;
+
   /* Doubly Linked List operations */
-  std::cout << "=== Doubly Linked List ===" << std::endl;
+  std::cout << "=== Double Doubly Linked List ===" << std::endl;
 
   // Initialize head and tail pointers for doubly linked list to nullptr
   // (emptylist)
-  DListNode *doublyHead = nullptr;
-  DListNode *doublyTail = nullptr;
+  DListNode<double> *doubleDHead = nullptr;
+  DListNode<double> *doubleDTail = nullptr;
 
   // Add three nodes to the beginning of the list
-  // After these operations, the list will contain: 10 <-> 20 <-> 30
-  addToStartOfDList(&doublyHead, &doublyTail, 30); // List becomes: 30
-  addToStartOfDList(&doublyHead, &doublyTail, 20); // List becomes: 20 <-> 30
-  addToStartOfDList(&doublyHead, &doublyTail,
-                    10); // List becomes: 10 <-> 20 <-> 30
+  // After these operations, the list will contain: 1.23 <-> 2.34 <-> 3.45
+  addToStartOfDList(&doubleDHead, &doubleDTail, 1.23); // List becomes: 1.23
+  addToStartOfDList(&doubleDHead, &doubleDTail,
+                    2.34); // List becomes: 1.23 <-> 2.34
+  addToStartOfDList(&doubleDHead, &doubleDTail,
+                    3.45); // List becomes: 1.23 <-> 2.34 <-> 3.45
 
   // Display the current state of the list in both directions
   // This demonstrates the bidirectional nature of doubly linked lists
   displayForwardDList(
-      doublyHead); // Outputs: List elements (forward): 10 <-> 20 <-> 30
+      doubleDHead); // Outputs: List elements (forward): 1.23 <-> 2.34 <-> 3.45
   displayBackwardDList(
-      doublyTail); // Outputs: List elements (backward): 30 <-> 20 <-> 10
+      doubleDTail); // Outputs: List elements (backward): 3.45 <-> 2.34 <-> 1.23
 
-  // Delete two nodes from the beginning of the list
+  // Delete nodes from the beginning and end of the list
   // This demonstrates removing elements from the doubly linked list
-  deleteAtStartOfDList(&doublyHead,
-                       &doublyTail); // Removes 10, list becomes: 20 <-> 30
+  deleteAtStartOfDList(
+      &doubleDHead,
+      &doubleDTail); // Removes 1.23, list becomes: 2.34 <-> 3.45
   displayForwardDList(
-      doublyHead); // Outputs: List elements (forward): 20 <-> 30
-  deleteAtStartOfDList(&doublyHead,
-                       &doublyTail); // Removes 20, list becomes: 30
-  displayForwardDList(doublyHead);   // Outputs: List elements (forward): 30
+      doubleDHead); // Outputs: List elements (forward): 2.34 <-> 3.45
+  deleteAtEndOfDList(&doubleDHead,
+                     &doubleDTail); // Removes 3.45, list becomes: 2.34
+  displayForwardDList(doubleDHead); // Outputs: List elements (forward): 2.34
 
   // Free all memory used by the doubly linked list
-  freeDList(&doublyHead, &doublyTail);
+  freeDList(&doubleDHead, &doubleDTail);
 
   return 0;
 }
